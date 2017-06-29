b653b1984bc9abd3cb6cc947fe6b7d86d94b4349
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-06-29 00:02:42 -0500

    Update test_latex.sh to prove a more complicated proposition

--- script/test_latex.sh
+++ script/test_latex.sh
@@ -1,2 +1,2 @@
 rm -rf resources/out.tex
-make && echo "A /\ B => B /\ A" | ./sequent --latex > resources/out.tex && cd resources && xelatex out.tex && open -a Preview out.pdf
+make && echo "((A /\\ B) => (B /\\ A)) /\\ ((B /\\ A) => (A /\\ B))" | ./sequent --latex > resources/out.tex && cd resources && xelatex out.tex && open -a Preview out.pdf
