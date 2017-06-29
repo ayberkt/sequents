8e3d7d0b680ec86559f13ef122e19b60b35aa434
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-06-28 22:57:02 -0500

    Get rid of the hacky testing script

--- test.sh
+++
@@ -1,9 +0,0 @@
-export IFS=$'\n'
-echo "Proofs should be found for these."
-for i in `cat test/valid.txt`;
-  do echo $i | ./sequent;
-done
-echo "Proofs should not be found for these"
-for i in `cat test/invalid.txt`;
-  do !(echo $i | ./sequent);
-done
