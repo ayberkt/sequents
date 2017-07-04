34f007c8cf5ec4f37d86e3d2b9215a392aa2f42e
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-04 09:21:08 -0500

    Update build.sh so it does not create the file `sequent` if compilation fails

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,2 @@
-echo "OS.Process.exit(OS.Process.failure)" | sml -m src/main.cm | grep "^[^'['.*]"
-printf 'sml @SMLload=sequent.x86-darwin $@\n' > sequent
+echo "OS.Process.exit(OS.Process.failure)" | sml -m src/main.cm && printf 'sml @SMLload=sequent.x86-darwin $@\n' > sequent
 chmod u+x sequent
