e26ce11f3bf6e8252ca6097d043bd1f24e766597
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-09 17:36:54 -0500

    Switch to pdflatex

--- script/test_latex.sh
+++ script/test_latex.sh
@@ -1,2 +1,2 @@
 rm -rf resources/out.tex
-bash script/build.sh && echo "((A /\\ B) => (B /\\ A)) /\\ ((B /\\ A) => (A /\\ B))" | ./sequent --latex > resources/out.tex && cd resources && xelatex out.tex && open -a Preview out.pdf
+bash script/build.sh && echo "((A /\\ B) => (B /\\ A)) /\\ ((B /\\ A) => (A /\\ B))" | ./sequent --latex > resources/out.tex && cd resources && pdflatex out.tex && open -a Preview out.pdf
