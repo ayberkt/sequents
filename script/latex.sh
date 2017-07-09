30cbd9abe776272d8e5905aaad9aaa0dbc35ae22
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-09 17:39:27 -0500

    Use build.sh instead of make

--- script/latex.sh
+++ script/latex.sh
@@ -3,5 +3,6 @@ if [ $# -eq 0 ]; then
   echo "Please provide exactly one proposition.";
   exit 1
 else
-  make && echo $1 | ./sequent --latex > resources/out.tex && cd resources && xelatex out.tex && open -a Preview out.pdf
+  ./script/build.sh && echo $1 | ./sequent --latex > resources/out.tex && cd resources && pdflatex out.tex && open -a Preview out.pdf
+
 fi
