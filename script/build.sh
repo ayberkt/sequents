42bf55b722984e99a1ff3482903c12a1a6e8792f
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-08 22:33:30 -0500

    Update build.sh so that it tries the linux architecture if darwin fails

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,6 @@
 echo "OS.Process.exit OS.Process.failure" | sml -m src/main.cm
 if [ $? -eq 0 ]; then
-  printf 'sml @SMLload=sequent.x86-darwin $@\n' > sequent
+  printf 'sml @SMLload=sequent.x86-darwin $@ || sml @SMLload=sequent.x86-linux $@\n' > sequent
   chmod u+x sequent
 else
   printf "Could not compile.\n"
