c41400f0493529b882870ae9393355b4c3274a59
Ayberk Tosun <ayberk.tosun@gmail.com>
2023-11-27 10:46:10 +0000

    Update the build script so that it works on Apple silicon machines

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,6 @@
 echo "OS.Process.exit OS.Process.failure" | sml -m src/main.cm
 if [ $? -eq 0 ]; then
-  printf 'sml @SMLload=sequent.x86-darwin $@\n' > sequent
+  printf 'sml @SMLload=sequent.amd64-darwin $@\n' > sequent
   chmod u+x sequent
 else
   printf "Could not compile.\n"
